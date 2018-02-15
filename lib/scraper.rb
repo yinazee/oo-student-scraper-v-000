8fec9dba69156c7a8604173843987e490fa244b4
Yina Zhu <yinagit@gmail.com>
2018-02-15 23:10:30 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,19 +28,32 @@ class Scraper
     end
     students
   end
-end
 
-def self.scrape_profile_page(profile_slug)
-  student = {}
-  profile_page = Nokogiri::HTML(profile)slug))
-  links = profile_page.css("social-icon-container").children.css("a").map do |url|
-    url.each do |links|
-      if link.include?("linkedin")
-        student[:linkedin] = link
-      elsif link.include?("twitter")
-        student[:twitter] = link
-      elsif link.include?("github")
-        student[:github] = link
+
+
+  def self.scrape_profile_page(profile)
+      student = {}
+      profile_page = Nokogiri::HTML(open(profile))
+      links = profile_page.css(".social-icon-container").children.css("a").map { |url| url.attribute('href').value}
+      links.each do |link|
+        if link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("github")
+          student[:github] = link
+        elsif link.include?("twitter")
+          student[:twitter] = link
+        else
+          student[:blog] = link
+        end
       end
+      # student[:twitter] = profile_page.css(".social-icon-container").children.css("a")[0].attribute("href").value
+      # # if profile_page.css(".social-icon-container").children.css("a")[0]
+      # student[:linkedin] = profile_page.css(".social-icon-container").children.css("a")[1].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[1]
+      # student[:github] = profile_page.css(".social-icon-container").children.css("a")[2].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[2]
+      # student[:blog] = profile_page.css(".social-icon-container").children.css("a")[3].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[3]
+      student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+      student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+      student
+    end
 
-end
+  end
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,37 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    # should take in an argument of a hash and use metaprogramming to assign the
+    # newly created student attributes and values in accordance with the key/value
+    # pairs of the hash. Use the #send method to acheive this.
+    # This method should also add the newly created student to the Student class' @@all array of all students.
+    student_hash.each do |k,v|
+      self.send("#{k}=", v)
+    end
+    @@all << self
   end
+
 
   def self.create_from_collection(students_array)
-    
-  end
-
-  def add_student_attributes(attributes_hash)
-    
-  end
-
-  def self.all
-    
+    # should iterate over the array of hashes and create a new individual student using each hash.
+    # This brings us to the #initialize method on our Student class.
+    students_array.each do |student_hash|
+      Student.new(student_hash)
   end
 end
 
+def add_student_attributes(attributes_hash)
+ attributes_hash.each do |attr, value|
+   self.send("#{attr}=", value)
+ end
+ self
+end
+
+  def self.all
+    @@all
+  end
+end
